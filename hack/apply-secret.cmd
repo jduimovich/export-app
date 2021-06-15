5f8df40e677b2ce89e75065b7a1fd2484265d41c
John Duimovich <jduimovich@gmail.com>
2021-06-15 00:18:50 -0400

    fix env refs

--- hack/apply-secret.cmd
+++ hack/apply-secret.cmd
@@ -9,12 +9,6 @@ echo "Secret being installed from ENV MY_GITHUB_USER and MY_GITHUB_TOKEN"
 echo "Secret Manifest is " %GIT_SECRET% 
 
 yq e ".stringData.username=\"%MY_GITHUB_USER%\"" %GIT_SECRET% | yq e ".stringData.password=\"%MY_GITHUB_TOKEN%\"" - | kubectl apply -f -
-
-echo "Username and password being installed from ENV MY_GITHUB_USERNAME and MY_GITHUB_EMAIL"
-yq e ".data.username=\"%MY_GITHUB_USERNAME%\"" %GIT_CONFIG% | yq e ".data.email=\"%MY_GITHUB_EMAIL%\"" - | kubectl apply -f -
-
-
-  
-  
+kubectl apply -f %GIT_CONFIG%
 
  
