8da6d6be69f689a11ca5aaf94b81e61e406203d5
John Duimovich <jduimovich@gmail.com>
2021-06-15 17:47:40 -0400

    secret and config map script

--- hack/apply-secret.cmd
+++ hack/apply-secret.cmd
@@ -2,13 +2,26 @@
 set CURRENT_DIR=%cd%
 set SCRIPT_DIR=%~dp0
 
-set GIT_SECRET=deploy-secret\gitsecret.yaml 
-set GIT_CONFIG=deploy-secret\gitconfig.yaml
+set MY_GITHUB_REPO=gitops-1
+
+set DEPLOY_GIT_SECRET=deploy-secret\gitsecret.yaml 
+set DEPLOY_GIT_CONFIG=deploy-secret\gitconfigmap.yaml
 
 echo "Secret being installed from ENV MY_GITHUB_USER and MY_GITHUB_TOKEN"
-echo "Secret Manifest is " %GIT_SECRET% 
+echo "Secret Manifest is " %DEPLOY_GIT_SECRET% 
 
-yq e ".stringData.username=\"%MY_GITHUB_USER%\"" %GIT_SECRET% | yq e ".stringData.password=\"%MY_GITHUB_TOKEN%\"" - | kubectl apply -f -
-kubectl apply -f %GIT_CONFIG%
+yq e ".stringData.username=\"%MY_GITHUB_USER%\"" %DEPLOY_GIT_SECRET% | yq e ".stringData.password=\"%MY_GITHUB_TOKEN%\"" - | kubectl apply -f -
+
+yq e ".data.username=\"%MY_GITHUB_USERNAME%\"" %DEPLOY_GIT_CONFIG% > tmp.yaml
+yq e ".data.email=\"%MY_GITHUB_EMAIL%\"" tmp.yaml > tmp2.yaml
+yq e ".data.repo=\"%MY_GITHUB_REPO%\"" tmp2.yaml > tmp.yaml
+kubectl apply -f tmp.yaml
+type tmp.yaml
+del tmp*.yaml
+
+
+
+ 
+
 
  
